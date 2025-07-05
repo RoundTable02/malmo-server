944bdd95f4bc258415d44f8a1e2c3de27ea4c4fa
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2025-07-06 02:10:54 +0900

    fix : 에러 코드 중복 문제 해결

--- src/main/java/makeus/cmc/malmo/adaptor/in/web/exception/ErrorCode.java
+++ src/main/java/makeus/cmc/malmo/adaptor/in/web/exception/ErrorCode.java
@@ -14,7 +14,7 @@ public enum ErrorCode {
     NO_SUCH_COUPLE_CODE(HttpStatus.BAD_REQUEST, 40002, "초대 코드가 존재하지 않습니다."),
     NO_SUCH_TERMS(HttpStatus.BAD_REQUEST, 40003, "약관이 존재하지 않습니다."),
     NO_SUCH_LOVE_TYPE(HttpStatus.BAD_REQUEST, 40004, "애착 유형이 존재하지 않습니다."),
-    NO_SUCH_LOVE_TYPE_QUESTION(HttpStatus.BAD_REQUEST, 40004, "애착 유형 질문이 존재하지 않습니다."),
+    NO_SUCH_LOVE_TYPE_QUESTION(HttpStatus.BAD_REQUEST, 40005, "애착 유형 질문이 존재하지 않습니다."),
 
     // 401 Unauthorized
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, 40100, "인증되지 않은 사용자입니다."),
