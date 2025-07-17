2927b4d75a5a4d9656030a709dc7cf53535250df
Choi wontak <76509639+RoundTable02@users.noreply.github.com>
2025-07-17 18:09:02 +0900

    refactor : 질문 조회 API에서 캐싱된 질문을 조회하도록 변경

--- src/main/java/makeus/cmc/malmo/application/service/LoveTypeQuestionService.java
+++ src/main/java/makeus/cmc/malmo/application/service/LoveTypeQuestionService.java
@@ -2,23 +2,30 @@ package makeus.cmc.malmo.application.service;
 
 import lombok.RequiredArgsConstructor;
 import makeus.cmc.malmo.application.port.in.GetLoveTypeQuestionsUseCase;
+import makeus.cmc.malmo.application.port.out.LoadLoveTypeQuestionDataPort;
 import makeus.cmc.malmo.application.port.out.LoadLoveTypeQuestionsPort;
 import makeus.cmc.malmo.domain.model.love_type.LoveTypeQuestion;
+import makeus.cmc.malmo.domain.model.love_type.LoveTypeQuestionData;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class LoveTypeQuestionService implements GetLoveTypeQuestionsUseCase {
 
-    private final LoadLoveTypeQuestionsPort loadLoveTypeQuestionsPort;
+    private final LoadLoveTypeQuestionDataPort loadLoveTypeQuestionDataPort;
 
     @Override
     public LoveTypeQuestionsResponseDto getLoveTypeQuestions() {
-        List<LoveTypeQuestion> loveTypeQuestions = loadLoveTypeQuestionsPort.loadLoveTypeQuestions();
+        Map<Long, LoveTypeQuestionData> map = loadLoveTypeQuestionDataPort.loadLoveTypeData();
+        List<LoveTypeQuestionData> loveTypeQuestions = map.values().stream()
+                        .sorted(Comparator.comparingInt(LoveTypeQuestionData::getQuestionNumber))
+                        .toList();
 
         List<LoveTypeQuestionDto> loveTypeQuestionDtoList = loveTypeQuestions.stream()
                 .map(l -> LoveTypeQuestionDto.builder()
